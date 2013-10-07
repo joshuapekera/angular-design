7d49f0214bb7537d1d93fb83035c28222b93fdb4
Wesley Cho <wesley.cho@gmail.com>
2013-10-06 22:42:42 -0700

    Fixed MainCtrl spec

--- tests/client/spec/controllers/main.spec.js
+++ tests/client/spec/controllers/main.spec.js
@@ -20,7 +20,7 @@ describe('MainCtrl', function () {
 
     $rootScope.sendLine();
 
-    expect(_($rootScope.sendLine).isFunction).toBe(true);
+    expect(_($rootScope.sendLine).isFunction()).toBe(true);
     expect($rootScope.lines.length).toBe(3);
     expect($rootScope.lines[2].text).toBe('foo');
   });
