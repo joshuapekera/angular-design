edf05123b06db5ca6dbf8d19acd087ac49bba78a
Wesley Cho <wesley.cho@gmail.com>
2013-10-21 23:09:19 -0400

    Implemented removal of user from database on socket disconnect

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -36,8 +36,17 @@ module.exports = {
         return res.json({ message: err });
       }
 
-      User.subscribe(req.socket);
-      this.publishUpdate();
+      req.listen(1);
+      User.introduce(req.socket, 1);
+      this.publishUpdate(req);
+
+      var self = this;
+
+      req.socket.on('disconnect', function () {
+        user.destroy(function () {
+          self.publish(req);
+        });
+      });
 
       return res.json({ user: user });
     });
@@ -53,17 +62,18 @@ module.exports = {
         return res.json({ message: err });
       }
 
-      this.publishUpdate();
+      this.publishUpdate(req);
 
       return res.json({
         line: line
       });
     });
   },
-  publishUpdate: function () {
-    return User.publishCreate({
-      users: User.findByConnected(true),
-      lines: Line.find().sort('createdAt desc').limit(50)
-    });
+  publishUpdate: function (req) {
+    return User.publish(req, [{ id: 1 }],
+      {
+        users: User.findByConnected(true),
+        lines: Line.find().sort('createdAt desc').limit(50)
+      });
   }
 };
