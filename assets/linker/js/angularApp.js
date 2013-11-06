db368e17254b1ba2815acd0ced55b3dc23db3235
Wesley Cho <wesley.cho@gmail.com>
2013-11-06 11:16:55 -0500

    Fixed karma.conf.js, fixed angular ui bootstrap reference in Gruntfile, added providers for angular ui bootstrap

--- assets/linker/js/angularApp.js
+++ assets/linker/js/angularApp.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('ChatApp', ['ui.bootstrap']);
+angular.module('ChatApp', ['ui.bootstrap'])
+  .config(function ($provide) {
+    $provide.decorator('accordionGroupDirective', function ($delegate) {
+      $delegate[0].templateUrl = '/linker/templates/accordion/accordion-group.html';
+
+      return $delegate;
+    });
+    $provide.decorator('modalBackdropDirective', function ($delegate) {
+      $delegate[0].templateUrl = '/linker/templates/modal/backdrop.html';
+      
+      return $delegate;
+    });
+    $provide.decorator('modalWindowDirective', function ($delegate) {
+      $delegate[0].templateUrl = '/linker/templates/modal/window.html';
+      
+      return $delegate;
+    });
+  });
