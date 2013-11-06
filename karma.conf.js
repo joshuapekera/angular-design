867d3e5f391d99404aa3a06db576975338b29dc8
Wesley Cho <wesley.cho@gmail.com>
2013-11-06 11:15:38 -0500

    Fixed karma.conf.js, fixed angular ui bootstrap reference in Gruntfile, added providers for angular ui bootstrap

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
       'assets/linker/bower_components/lodash/dist/lodash.js',
       'assets/linker/bower_components/angular/angular.js',
       'assets/linker/bower_components/angular-mocks/angular-mocks.js',
-      'assets/linker/bower_components/angular-bootstrap/ui-bootstrap.js',
+      'assets/linker/bower_components/angular-bootstrap/dist/ui-bootstrap-0.6.0-SNAPSHOT.js',
       'assets/linker/js/angularApp.js',
       'assets/linker/js/controllers/main.js',
       'tests/client/spec/**/*.spec.js'
